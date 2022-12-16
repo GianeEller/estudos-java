dad2ca5f53799f0fd38ea20b3a070ac5f7b1b15d
Giane Eller <gianeemg@gmail.com>
2022-12-16 15:24:38 -0300

    exemplos de Operadores

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,20 @@
 public class TiposVariaveis {
+    /**
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        String meuNome = "Giane"
+
+        double salarioMinimo = 2500;
+
+        int numero = 5;
+        numero = 10;
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
     }
 }
