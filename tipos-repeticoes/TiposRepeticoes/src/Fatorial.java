d45a036721813ed6f87bf41aaada37b10e75586f
Giane Eller <gianeemg@gmail.com>
2022-12-20 16:21:48 -0300

    exercicio para resolver Fatorial

---
+++ tipos-repeticoes/TiposRepeticoes/src/Fatorial.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/*
+ * faça um programa que calcule o fatoria de numero inteiro fornecido pelo usuário
+ * ex.:  5! = 120 (5*4*3*2*1)
+ */
+
+public class Fatorial {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Fatorial: ");
+        int fatorial = scan.nextInt();
+
+        int multiplicacao = 1;
+
+        System.out.println(fatorial + "! = ");
+
+
+            multiplicacao = multiplicacao * i;
+        }
+        System.out.println(multiplicacao);
+    }}
